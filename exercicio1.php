60f0939e736e6c344a70aa3ab1516cf3f60e79b7
João Victor <engs-joaofortkamp@camporeal.edu.br>
2026-07-28 16:38:06 -0300

    ex3 correçao

--- exercicio1.php
+++ exercicio1.php
@@ -9,8 +9,7 @@ if ($idade >= 18) {
     echo "Você é menor de idade\n";
 }
 
-//  2. IF/ELSE - dinheiro 
-
+// ===== 2. IF/ELSE - dinheiro =====
 $dinheiro = 15000;
 
 if ($dinheiro < 1000) {
@@ -24,3 +23,33 @@ if ($dinheiro < 1000) {
 } else {
     echo "Elon Musk\n";
 }
+
+//3. Operação com switch
+$numero1 = 10;
+$numero2 = 5;
+$operacao = "+";
+
+switch ($operacao) {
+    case "+":
+        echo $numero1 + $numero2, "\n";
+        break;
+    case "-":
+        echo $numero1 - $numero2, "\n";
+        break;
+    case "*":
+        echo $numero1 * $numero2, "\n";
+        break;
+    case "/":
+        echo $numero1 / $numero2, "\n";
+        break;
+    default:
+        echo "Operação inválida\n";
+}
+
+// 4 Números pares de 1 até 100
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 2 == 0) {
+        echo $i, " ";
+    }
+}
+echo "\n";
